e3932cfb6c1f52d00348ce310cd0ce5a88faa1e7
Lefundes <angelolefundes@yahoo.com.br>
2024-05-22 15:19:06 -0300

    Retirando caracter especial da senha e letras minúsculas

--- src/main/java/com/ecocursos/util/PasswordGenerator.java
+++ src/main/java/com/ecocursos/util/PasswordGenerator.java
@@ -5,12 +5,12 @@ import java.util.Random;
 
 public class PasswordGenerator {
 
-    private static final String CHAR_LOWER = "abcdefghijklmnopqrstuvwxyz";
+    private static final String CHAR_LOWER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
     private static final String CHAR_UPPER = CHAR_LOWER.toUpperCase();
     private static final String NUMBER = "0123456789";
     private static final String OTHER_CHAR = "!@#$%&*()_+-=[]?";
 
-    private static final String PASSWORD_ALLOW_BASE = CHAR_LOWER + CHAR_UPPER + NUMBER + OTHER_CHAR;
+    private static final String PASSWORD_ALLOW_BASE = CHAR_LOWER + CHAR_UPPER + NUMBER;
 
     private static SecureRandom random = new SecureRandom();
 
