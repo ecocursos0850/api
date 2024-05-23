d7cdf41a645f452b39d1ed18f42a332d96967fcc
Lefundes <angelolefundes@yahoo.com.br>
2024-05-23 19:02:15 -0300

    Correção do insert de pergunta

--- src/main/java/com/ecocursos/ecocursos/models/Pergunta.java
+++ src/main/java/com/ecocursos/ecocursos/models/Pergunta.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class Pergunta {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Column(columnDefinition = "TEXT")
     private String descricao;
