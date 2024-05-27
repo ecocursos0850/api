17523176a2128fedb7111931f5221b7baf611d8d
Lefundes <angelolefundes@yahoo.com.br>
2024-05-27 09:46:03 -0300

    Correção pergunta GenerationType

--- src/main/java/com/ecocursos/ecocursos/models/Resposta.java
+++ src/main/java/com/ecocursos/ecocursos/models/Resposta.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 public class Resposta {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String resposta;
     private boolean correta;
