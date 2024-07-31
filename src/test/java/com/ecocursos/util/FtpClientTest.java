b68a38e26b5351a876c011e3bc7662242f895ce9
erickand16 <cerickandradebank@gmail.com>
2024-07-31 19:28:34 -0300

    teste de envio de certificado

---
+++ src/test/java/com/ecocursos/util/FtpClientTest.java
@@ -0,0 +1,14 @@
+package com.ecocursos.util;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = FtpClient.class)
+public class FtpClientTest {
+    
+    @Test
+    void testSendFile() throws Exception {
+
+
+    }
+}
